434addb428edbb601d4a4f16f0b2a8d9877d7811
Matt-Mc <30872077+Matt-Mc@users.noreply.github.com>
2017-10-31 18:54:54 -0400

    Twitter Bot

---
+++ README.txt
@@ -0,0 +1,5 @@
+Twitter bot that searches twitter for a keyword (in this case, "BlizzCon"), takes a tweet containing that keyword then makes a post with that tweet. 
+
+Posts once every 15 minutes 
+
+Bot Twitter: https://twitter.com/mattprojectema1
---
+++ bot.js
@@ -0,0 +1,33 @@
+console.log("the bot is starting");
+
+var Twit = require('twit');
+var module = require('./module');
+var T = new Twit(module);
+
+
+setInterval(getTweet, 1000*900)
+
+function getTweet(){
+  T.get('search/tweets', { q: 'BlizzCon since:2011-09-01', count: 1 }, function(err, data, response) {
+    var tweets = data.statuses;
+    for(var i = 0; i < tweets.length; i++){
+      tweetIt(tweets[i].text);
+    }
+  })
+}
+
+function tweetIt(txt){
+  var tweet = {
+    status: txt
+  }
+
+  T.post('statuses/update', tweet, tweeted);
+
+  function tweeted(err, data, response){
+    if (err){
+      console.log("something went wrong!");
+    }else{
+      console.log("It worked!");
+    }
+  }
+}
